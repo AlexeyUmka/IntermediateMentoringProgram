e87832ea9be642ce176f59a8733e28d3a6757e06
KYIV\Oleksii_Yurchenko <Oleksii_Yurchenko@epam.com>
2021-11-17 22:51:45 +0200

    Task 4 scratch

--- MultiThreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ MultiThreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,11 +10,13 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
+        private static int _threadsAmount = 10;
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
@@ -25,10 +27,38 @@ namespace MultiThreading.Task4.Threads.Join
             Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
 
             Console.WriteLine();
-
-            // feel free to add your code
+            
+            DecrementUsingThread(_threadsAmount);
+            DecrementUsingThreadPool(_threadsAmount);
 
             Console.ReadLine();
+        }
+        
+        static void DecrementUsingThread(object count)
+        {
+            var countInt = count as int?;
+            if (countInt > 0)
+            {
+                var myThread = new Thread(DecrementUsingThread);
+                Console.WriteLine(countInt);
+                countInt--;
+                myThread.Start(countInt);
+                myThread.Join();
+            }
+        }
+        private static readonly ManualResetEvent ResetEvent = new(false);
+        static void DecrementUsingThreadPool(object count)
+        {
+            ResetEvent.Reset();
+            var countInt = count as int?;
+            if (countInt > 0)
+            {
+                Console.WriteLine(countInt);
+                countInt--;
+                ResetEvent.Set();
+                ThreadPool.QueueUserWorkItem(DecrementUsingThreadPool, countInt);
+                ResetEvent.WaitOne();
+            }
         }
     }
 }
